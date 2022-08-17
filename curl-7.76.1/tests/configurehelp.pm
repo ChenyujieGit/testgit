c0ba8b87ec347f065bb916b8baacab36099dbe23
linuxgit <1294708572@qq.com>
2022-08-16 23:46:27 -0700

    finished

---
+++ curl-7.76.1/tests/configurehelp.pm
@@ -0,0 +1,23 @@
+# This is a generated file.  Do not edit.
+
+package configurehelp;
+
+use strict;
+use warnings;
+use Exporter;
+
+use vars qw(
+    @ISA
+    @EXPORT_OK
+    $Cpreprocessor
+    );
+
+@ISA = qw(Exporter);
+
+@EXPORT_OK = qw(
+    $Cpreprocessor
+    );
+
+$Cpreprocessor = 'arm-oe-linux-gnueabi-gcc --sysroot=/home/chenyujie/learngit/sim_crosscompile/sysroots/mdm9607-perf -E -isystem /home/chenyujie/learngit/openssl-1.1.1c/output/include';
+
+1;
