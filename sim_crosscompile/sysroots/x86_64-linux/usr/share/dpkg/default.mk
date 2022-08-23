fd2cade086373a54143594a49926cf9b7c463696
linuxgit <1294708572@qq.com>
2022-08-23 02:11:13 -0700

    new

--- sim_crosscompile/sysroots/x86_64-linux/usr/share/dpkg/default.mk
+++
@@ -1,8 +0,0 @@
-# Include all the Makefiles that define variables that can be useful
-# within debian/rules
-
-dpkg_datadir = /home/mk-build1/haotian/LE11_LATEST_AP/oe-core/build/tmp-glibc/sysroots/x86_64-linux/usr/share/dpkg
-include $(dpkg_datadir)/architecture.mk
-include $(dpkg_datadir)/buildflags.mk
-include $(dpkg_datadir)/pkg-info.mk
-include $(dpkg_datadir)/vendor.mk
